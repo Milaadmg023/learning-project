9e75e330c37787deab50f8ba854f8fd28e22ac49
Milaadmg023 <132762516+Milaadmg023@users.noreply.github.com>
2024-03-17 08:35:07 +0330

    properties page : adding images & components

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,7 +1,21 @@
-const Properties = () => {
-  return (
-    <div>Porperties Page</div>
-  )
-}
+"use client"
+import Property from "@/components/Property";
+import Properties from "@/properties.json";
 
-export default Properties;
+const PropertiesPage = () => {
+  return (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {Properties.map((property , index) => {
+            return(
+              <Property property={property} key={index}/>
+            )
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PropertiesPage;
